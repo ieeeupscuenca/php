fa19a01778a334e4512a496f1ed4589110091fda
root <root@localhost.bvega>
2019-04-28 15:32:42 -0500

    Creando login

---
+++ public/controladores/login.php
@@ -0,0 +1,30 @@
+<?php
+
+    session_start();
+
+    include '../../config/conexionBD.php';
+
+    $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;
+
+    $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
+
+    $sql = "SELECT count(*) as login  
+            FROM usuario 
+            WHERE usu_correo = '$usuario' and 
+                  usu_password = MD5('$contrasena')";
+
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    
+
+    if ($row["login"] == 1) {
+        $_SESSION['isLogged'] = TRUE;
+        print("hola mundo");
+    
+    }else{
+        header("Location: ../vista/login.html");
+    }
+
+    $conn->close();
+
+?>
